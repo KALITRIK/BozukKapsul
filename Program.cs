f7c82a7db1b79aad1f66cc51607ecd700038a19f
Kalitrik <70946597+KALITRIK@users.noreply.github.com>
2021-06-10 11:45:35 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 TC = new Class1();
+            Console.WriteLine(TC.Tcgir());
+            Console.WriteLine(TC.TC);
+            Console.ReadLine();
+
+
+        }
+    }
+}
